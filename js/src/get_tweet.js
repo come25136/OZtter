8d0818dc6e274967bd6b98b1b426e0c78aa4e95c
come25136 <come25136@gmail.com>
2016-10-29 20:29:54 +0900

    中心からブワワーってなるようにした
    メンションとURLのハイパーリンクを機能するようにした

--- js/src/get_tweet.js
+++ js/src/get_tweet.js
@@ -46,16 +46,28 @@ function Init(limit, data) {
     return validateJSON(data.data);
 }
 
+var w_size = 48;
+
 function add_tweet(id, icon_url, value) {
-    $(TweetDisplay).append('<div class="tweet" id="' + id + '" style="top:' + Math.floor(Math.random() * (window.innerHeight - 100)) + 'px; left:' + Math.floor(Math.random() * (window.innerWidth - 300)) + 'px;">'
+    var wh = window.innerHeight - 104;
+    var ww = window.innerWidth - 300;
+
+    $(TweetDisplay).append('<div class="tweet" id="' + id + '" style="top:' + calculation(wh / 100 * w_size, wh, 104) + 'px; left:' + calculation(ww / 100 * w_size, ww, 300) + 'px;">'
         + '<div class="title_bar">'
+        + 'OZtter Message'
         + '<button class="close" title="閉じる">'
         + '<span class="cross"></span>'
         + '</button>'
         + '</div>'
         + '<img class="icon"  align="left" src="' + icon_url + '">'
-        + '<p class="value">' + value.replace(/\r\n?/g, "<br>") + '</p>'
+        + '<p class="value">' + tweet_value(value) + '</p>'
         + '</div>');
+
+    $('.tweet').draggable({handle: '.title_bar'});
+
+    if (0 < w_size) {
+        w_size--;
+    }
 }
 
 // JSON の評価を行う、JSON.parseでエラーになる場合は、jsとしてevalする
@@ -79,6 +91,17 @@ function validateJSON(text) {
 
     console.log("WARN. As a result of JSON.parse, a trivial problem has occurred");
     return obj; // repaired
+}
+
+function tweet_value(value) {
+    return value
+        .replace(/(https?:\/\/[\x21-\x7e]+)/gi, "<a href='$1'>$1</a>") // メンションのURLと干渉するため、必ず一番最初にこの処理を書くこと
+        .replace(/@(\w+)/g, '<a href="https://twitter.com/$1">@$1</a>')
+        .replace(/\r\n?/g, "<br>");
+}
+
+function calculation(min, max, size) {
+    return Math.random() * ((max - min) - min) + min;
 }
 
 function m_random(length) {
